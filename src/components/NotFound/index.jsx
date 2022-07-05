5af4f8a279b1c137d6de535e5f33465cf7134737
luadanchu <lu.adanchu@hotmail.com>
2022-07-05 15:22:16 -0300

    page 404

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import './style.css'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Ups..Something goes wrong</h2>
-        <h3>Page not found</h3>
-        <button><Link to='/'>Return</Link></button>
+    <div className='page_404'>
+      <div className='four_zero_four_bg'>
+        <h2>404</h2>
+      </div>
+      <div className='contant_box_404'>
+        <h3 className='h2'>Look like you're lost</h3>
+        <p>The page you are looking for is not avaible!</p>
+        <button className='link_404'><Link to='/'>Return</Link></button>
+      </div>
     </div>
   )
 }
